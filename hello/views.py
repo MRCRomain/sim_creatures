7c1cb0585d0564830ae9ac90cbc155f6bcc39afb
MRC_Romain <miorcec@MBP-de-Miorcec.attlocal.net>
2019-07-03 15:46:59 -0500

    Created 3 pages (Home, Playground, settings)

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,24 @@
+import re
+from datetime import datetime
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Test view Django.")
+    return render(request, "hello/home.html")
+
+
+def hello_there(request, name):
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+
+def playground(request):
+    return render(request, "hello/playground.html")
+
+def parameters(request):
+    return render(request, "hello/parameters.html")
