86ab9d3e92bd9a5a29b65ca42d1170cc1d0b5376
MRC_Romain <miorcec@MBP-de-Miorcec.attlocal.net>
2019-07-06 17:26:10 -0500

    Setup Channels

--- sim_creatures/routing.py
+++ sim_creatures/routing.py
@@ -1,5 +1,20 @@
-from channels.routing import ProtocolTypeRouter
+from django.conf.urls import url
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack, AnonymousUser
+from hello.consumers import send_data
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
+
+
 
 application = ProtocolTypeRouter({
     # Empty for now (http->django views is added by default)
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                [ 
+                     url(r"^playground/$",send_data)
+                ]
+            )
+        )
+    )
 })
